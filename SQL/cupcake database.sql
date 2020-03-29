c3133e0c75683a5085223373df245e56ec37942a
A-R-G-Wagner <60380845+A-R-G-Wagner@users.noreply.github.com>
2020-03-29 14:08:25 +0200

    Flere orders m. content

--- SQL/cupcake database.sql
+++ SQL/cupcake database.sql
@@ -23,12 +23,12 @@ CREATE TABLE Users (
     Phone varchar(20),
     Address varchar (200),
     PRIMARY KEY (UserId),
-    FOREIGN KEY (RoleId) REFERENCES Roles (RoleID)
+    FOREIGN KEY (RoleId) REFERENCES Roles (RoleId)
 );
 ALTER TABLE Users auto_increment=1000;
 
-INSERT INTO Users (Email, UserPassword, Fullname, Phone, Address)
-Values ("Alice@Cooper.com","pass1234", "Alice Cooper", "60607070", "Nørgaardsvej 30, DK-2800 Kgs. Lyngby");
+INSERT INTO Users (Email, UserPassword)
+Values ("Alice@Cooper.com","pass1234");
 INSERT INTO Users (Email, UserPassword)
 Values ("Frank@Zappa.com","pass1234");
 INSERT INTO Users (Email, UserPassword)
@@ -113,7 +113,9 @@ CREATE TABLE Orders (
 ALTER TABLE Orders auto_increment=2500;
 
 INSERT INTO Orders (UserId,OrderPrice)
-VALUES ((SELECT UserId FROM Users WHERE Email = "Alice@Cooper.com"),84);
+VALUES ((SELECT UserId FROM Users WHERE Email = "Alice@Cooper.com"),177);
+INSERT INTO Orders (UserId,OrderPrice)
+VALUES ((SELECT UserId FROM Users WHERE Email = "Frank@Zappa.com"),74);
 
 CREATE TABLE OrderContent (
 	OrderId int,
@@ -125,9 +127,14 @@ CREATE TABLE OrderContent (
     FOREIGN KEY (Top) REFERENCES Top (TopType),
     FOREIGN KEY (Bottom) REFERENCES Bottom (BottomType)
 );
-INSERT INTO OrderContent (OrderId,Top,Bottom,Quantity)
+INSERT INTO OrderContent (orderId,top,bottom,quantity)
 VALUES (2500, "Orange", "Almond", 2);
-INSERT INTO OrderContent (OrderId,Top,Bottom,Quantity)
+INSERT INTO OrderContent (orderId,top,bottom,quantity)
 VALUES (2500, "Blue Cheese", "Almond", 1);
-INSERT INTO OrderContent (OrderId,Top,Bottom,Quantity)
+INSERT INTO OrderContent (orderId,top,bottom,quantity)
 VALUES (2500, "Chocolate", "Almond", 4);
+INSERT INTO OrderContent (orderId,top,bottom,quantity)
+VALUES (2501,"Blueberry", "Vanilla", 3);
+INSERT INTO OrderContent (orderId,top,bottom,quantity)
+VALUES (2501,"Strawberry", "Chocolate", 4),
+	   (2501,"Strawberry", "Vanilla", 4);
