8c90e06fd34f6d3d5d3c31d18e75f83eea26d369
joakimkss <joakim.kss@gmail.com>
2020-03-30 17:30:30 +0200

    Kode refactor og javaDocs

--- src/main/java/DBAccess/DBConnector.java
+++ src/main/java/DBAccess/DBConnector.java
@@ -1,39 +1,57 @@
 package DBAccess;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.Reader;
 import java.sql.*;
 
-/**
- * 
- * @author Per, Joakim, Frederik
- */
 public class DBConnector {
-
+    private static String url, username, password, nameDB;
+    private static Connection conn = null;
     //Class for connecting to DB and performing queries.
 
-    private static Connection conn() {
-
-        try {
-            String driver, url, username, password, nameDB;
-            nameDB = "cupcake";
-            driver = ""/*""com.mysql.cj.jdbc.Driver"*/;
-            url = "jdbc:mysql://localhost:3306/" + nameDB + "?verifyServerCertificate=false&useSSL=true&serverTimezone=UTC";
-            username = "admin";
-            password = "cupcakeworld";
-            Class.forName(driver);
-
+    /**
+     *
+     * @return Connection
+     * @throws ClassNotFoundException Java
+     * @throws SQLException MySQL
+     */
+    private static Connection conn() throws ClassNotFoundException, SQLException {
+        if (conn == null) {
+            setDBCredentials();
+            Class.forName("com.mysql.cj.jdbc.Driver");
             //System.out.println("\n--------------- MySQL ---------------");
-            Connection conn = DriverManager.getConnection(url, username, password);
+            conn = DriverManager.getConnection(url, username, password);
             //System.out.println("Connected");
-            return conn;
-        } catch (Exception e) {
-            System.out.println("Java Exception: " + e);
         }
-        return null;
+        return conn;
     }
 
+    /**
+     * Specifies database credentials
+     */
+    public static void setDBCredentials() {
+        String deployed = System.getenv("DEPLOYED");
+        if (deployed != null) {
+            url = System.getenv("JDBC_CONNECTION_STRING");
+            username = System.getenv("JDBC_USER");
+            password = System.getenv("JDBC_PASSWORD");
+            System.out.println("DEPLOYED: PROD selected");
+            System.out.println("----------------------------");
+            System.out.println(url);
+            System.out.println(username);
+            System.out.println(password);
+            System.out.println("----------------------------");
+        } else {
+            nameDB = "cupcake";
+            url = "jdbc:mysql://localhost:3306/" + nameDB + "?verifyServerCertificate=false&useSSL=true&serverTimezone=CET";
+            username = "admin";
+            password = "cupcakeworld";
+            System.out.println("DEPLOYED: DEV selected");
+        }
+    }
+
+    /**
+     *
+     * @param query Specified MySQL query
+     */
     public static void updateSQL(String query) {
         Statement st = null;
         try {
@@ -61,6 +79,11 @@ public class DBConnector {
         }
     }
 
+    /**
+     *
+     * @param query Specified MySQL query
+     * @return ResultSet containing data from database
+     */
     public static ResultSet querySQL(String query) {
         Statement st;
         ResultSet rs = null;
@@ -80,7 +103,12 @@ public class DBConnector {
         return rs;
     }
 
-    public static boolean sqlGuard() {
+    /**
+     *
+     * @return Validation of database connection
+     * @throws ClassNotFoundException Java
+     */
+    public static boolean sqlGuard() throws ClassNotFoundException {
         try {
             if (!DBConnector.conn().isValid(2)) {
                 System.out.println("Connection timeout! Aborting operation!");
@@ -89,7 +117,6 @@ public class DBConnector {
         } catch (SQLException e) {
             System.out.println(e);
         }
-
         return true;
     }
 }
